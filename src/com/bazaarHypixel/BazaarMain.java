61c50939952fcd1d096ae9db20460d3bd0de7a6b
nikijul007 <nikos01@gmx.net>
2020-03-19 19:59:02 +0100

    Test zurück

--- src/com/bazaarHypixel/BazaarMain.java
+++ src/com/bazaarHypixel/BazaarMain.java
@@ -3,8 +3,6 @@ package com.bazaarHypixel;
 public class BazaarMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//Dies ist ein Test
-		//Dies auch xD
 	}
 
 }
